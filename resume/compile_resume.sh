839eb6f4ac6ecf3f73a8b9c7c0c6773e60c1fd1f
Onur Ferhat <tiendan@gmail.com>
2016-01-21 09:29:55 +0100

    New Awesome latex template for CV

--- resume/compile_resume.sh
+++ resume/compile_resume.sh
@@ -4,12 +4,11 @@
 #rm latex/resume.bcf
 #rm latex/resume.out
 #rm latex/resume.run.xml
-rm resume.pdf
+rm onurferhat.pdf
 cd latex
-./biber resume
-xelatex resume.xtx
+xelatex onurferhat.tex
 cd ..
-cp latex/resume.pdf .
+cp latex/onurferhat.pdf .
 #rm -rf out
 #hackmyresume BUILD onurferhat-resume.json TO out/onurferhat-resume.all -t awesome
 #cd out/src/latex/examples
